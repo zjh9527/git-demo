75a0876ae88e2a7b0c497f70ab34513b10387fc2
zhangjianhua <137253690@qq.com>
2023-03-12 13:56:00 +0800

    test hot-fix first commit

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -8,6 +8,7 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hot-fix");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
