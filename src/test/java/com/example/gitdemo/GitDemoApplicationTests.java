da1570123eafa6a1a62aa1ca61b4f8853a60f8ca
zhangjianhua <137253690@qq.com>
2023-03-12 14:12:55 +0800

    test master cal max substring len commit

--- src/test/java/com/example/gitdemo/GitDemoApplicationTests.java
+++ src/test/java/com/example/gitdemo/GitDemoApplicationTests.java
@@ -10,4 +10,43 @@ class GitDemoApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testStringSplitV1() {
+        String str = "aa a aaa aaaa a aaa aaa aaaa aaaaaaaa aaa";
+        final String[] s = str.split(" ");
+        int len = 0;
+
+        for (String value : s) {
+            if (" ".equals(value)) {
+                continue;
+            }
+
+            if (value.length() > len) {
+                len = value.length();
+            }
+        }
+
+        System.out.println(len);
+    }
+
+    @Test
+    void testMaxSubLengthV2() {
+        String str = "aa a aaa aaaa a aaa aaa aaaa aaaaaaaa aaa ";
+
+        int len = 0;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                count++;
+            } else {
+                if (len < count) {
+                    len = count;
+                }
+                count = 0;
+            }
+        }
+
+        System.out.println(len);
+    }
+
 }
